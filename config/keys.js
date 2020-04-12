33eb4d8794e859240ed2ce77e3c56b05582e49e6
karthikrv93 <karthik.rv93@gmail.com>
2020-04-11 21:37:23 -0700

    add to database

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-dbPassword = 'mongodb://localhost:27017/trivia';
+dbPassword = "mongodb://nrkarthi:August2019!asu@ds257507.mlab.com:57507/trivia";
+//'mongodb://localhost:27017/trivia';
 
 module.exports = {
-    mongoURI: dbPassword
+  mongoURI: dbPassword,
 };
